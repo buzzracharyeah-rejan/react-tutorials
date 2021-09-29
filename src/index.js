9387849e6732ab54f4d16be13578ea147b7b796b
buzzracharyeah rejan <rejandev@gmail.com>
2021-09-29 13:28:33 +0545

    JSX rules

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+    return (
+        <>
+            <div>
+                <h1>hello world!</h1>
+                <ul>
+                    <li>
+                        <a href="#">hello world</a>
+                    </li>
+                    <img src="" alt="" />
+                </ul>
+            </div>
+        </>
+    );
+}
+
+// const Greeting = () => {
+//     return React.createElement('div', {}, React.createElement('h1', {}, 'something went wrong!'));
+// }
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
